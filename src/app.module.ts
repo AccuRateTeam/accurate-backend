36964411bd1146262b7fbdbb2a4ebb362b4b1970
Niklas <niklas@nikkl.me>
2022-03-11 20:02:21 +0100

    install rate limiter
    Signed-off-by: Niklas <niklas@nikkl.me>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AuthzModule } from './authz/authz.module';
-import { UserModule } from './user/user.module';
-import { EventModule } from './event/event.module';
+import {Module} from '@nestjs/common';
+import {AuthzModule} from './authz/authz.module';
+import {UserModule} from './user/user.module';
+import {EventModule} from './event/event.module';
 
 @Module({
-  imports: [AuthzModule, UserModule, EventModule],
-  controllers: [],
-  providers: [],
+    imports: [
+        AuthzModule,
+        UserModule,
+        EventModule
+    ],
+    controllers: [],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
